32bddd21d41088e3c084620d612759c23467f36a
igor <iggorsantos987@gmail.com>
2024-04-24 21:58:07 -0300

    https://github.com/bttandrade/Sistema_De_Controle.git

--- firebase.js
+++ firebase.js
@@ -4,12 +4,13 @@ import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
 
 const firebaseConfig = {
-    apiKey: "sua-api-key",
-    authDomain: "seu-auth-domain",
-    projectId: "seu-project-id",
-    storageBucket: "seu-storage-bucket",
-    messagingSenderId: "seu-messaging-sender-id",
-    appId: "seu-app-id"
+    apiKey: "AIzaSyD5NFLOufex7Gylqd4RSE7V8icYXQqh93Y",
+    authDomain: "sistema-de-controle-5bc09.firebaseapp.com",
+    databaseURL: "https://sistema-de-controle-5bc09-default-rtdb.firebaseio.com",
+    projectId: "sistema-de-controle-5bc09",
+    storageBucket: "sistema-de-controle-5bc09.appspot.com",
+    messagingSenderId: "53243518833",
+    appId: "1:53243518833:web:084cafc5d7e72d80e7e0d0"
 };
 
 
