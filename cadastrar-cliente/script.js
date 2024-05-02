eb161af9285c2560e2fc654457b895d1fac8e2e9
DaviOliveira18 <134347042+DaviOliveira18@users.noreply.github.com>
2024-05-02 20:36:07 -0300

    Cadastro de clientes melhorado

--- cadastrar-cliente/script.js
+++ cadastrar-cliente/script.js
@@ -6,10 +6,50 @@ const sNome = document.querySelector('#m-nome');
 const sCidade = document.querySelector('#m-cidade');
 const sEmail = document.querySelector('#m-email');
 const sTelefone = document.querySelector('#m-telefone');
+const sCpf = document.querySelector('#m-cpf');
 const btnSalvar = document.querySelector('#btnSalvar');
 const addNew = document.querySelector('#new');
 
 let itemId;
+
+// Função para validar CPF
+function validarCPF(cpf) {
+  cpf = cpf.replace(/[^\d]+/g, '');    
+  if (cpf === '') return false; 
+  // Elimina CPFs invalidos conhecidos    
+  if (cpf.length !== 11 || 
+      cpf === '00000000000' || 
+      cpf === '11111111111' || 
+      cpf === '22222222222' || 
+      cpf === '33333333333' || 
+      cpf === '44444444444' || 
+      cpf === '55555555555' || 
+      cpf === '66666666666' || 
+      cpf === '77777777777' || 
+      cpf === '88888888888' || 
+      cpf === '99999999999') {
+      return false;
+  }       
+  // Valida 1o digito 
+  let add = 0;    
+  for (let i=0; i < 9; i ++)       
+      add += parseInt(cpf.charAt(i)) * (10 - i);  
+  let rev = 11 - (add % 11);  
+  if (rev === 10 || rev === 11)     
+      rev = 0;    
+  if (rev !== parseInt(cpf.charAt(9)))     
+      return false;       
+  // Valida 2o digito 
+  add = 0;    
+  for (let i = 0; i < 10; i ++)        
+      add += parseInt(cpf.charAt(i)) * (11 - i);  
+  rev = 11 - (add % 11);  
+  if (rev === 10 || rev === 11) 
+       rev = 0;    
+  if (rev !== parseInt(cpf.charAt(10)))
+       return false;       
+  return true;   
+}
 
 async function openModal(edit = false, id = null) {
   modal.classList.add('active');
@@ -27,6 +67,7 @@ async function openModal(edit = false, id = null) {
       sCidade.value = item.cidade;
       sEmail.value = item.email;
       sTelefone.value = item.telefone;
+      sCpf.value = item.cpf;
       itemId = id;
     }
   } else {
@@ -34,6 +75,7 @@ async function openModal(edit = false, id = null) {
     sCidade.value = '';
     sEmail.value = '';
     sTelefone.value = '';
+    sCpf.value = '';
     itemId = null;
   }
 }
@@ -64,7 +106,12 @@ async function updateItem(item) {
 btnSalvar.onclick = async (e) => {
   e.preventDefault();
 
-  if (sNome.value == '' || sCidade.value == '' || sEmail.value == '' || sTelefone.value == '') {
+  if (sNome.value == '' || sCidade.value == '' || sEmail.value == '' || sTelefone.value == '' || sCpf.value == '') {
+    return;
+  }
+
+  if (!validarCPF(sCpf.value)) {
+    alert('CPF inválido. Por favor, insira um CPF válido.');
     return;
   }
 
@@ -72,7 +119,8 @@ btnSalvar.onclick = async (e) => {
     nome: sNome.value,
     cidade: sCidade.value,
     email: sEmail.value,
-    telefone: sTelefone.value
+    telefone: sTelefone.value,
+    cpf: sCpf.value
   };
 
   if (itemId) {
@@ -98,6 +146,7 @@ async function loadItens() {
       <td>${item.cidade}</td>
       <td>${item.email}</td>
       <td>${item.telefone}</td>
+      <td>${item.cpf}</td>
       <td class="acao">
         <button class="editBtn"><i class='bx bx-edit' ></i></button>
       </td>
